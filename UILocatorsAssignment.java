504c8fab869007497654b0fab625cadde2baf149
abinayaavel <69348023+abinayaavel@users.noreply.github.com>
2025-07-24 14:34:18 +0530

    complete airlines site end2end automated

---
+++ UILocatorsAssignment.java
@@ -0,0 +1,30 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+
+public class UILocatorsAssignment {
+
+    public static void main(String[] args) {
+        
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/angularpractice/");
+        driver.manage().window().maximize();
+
+        driver.findElement(By.name("name")).sendKeys("Daya");
+        driver.findElement(By.name("email")).sendKeys("daya.kiki15@yahoo.com");
+        driver.findElement(By.cssSelector("input#exampleInputPassword1")).sendKeys("gayu23071990");
+        driver.findElement(By.cssSelector("input#exampleCheck1")).click();
+
+        Select option = new Select(driver.findElement(By.cssSelector("#exampleFormControlSelect1")));
+        option.selectByVisibleText("Female");
+
+        driver.findElement(By.cssSelector("input#inlineRadio2")).click();
+        driver.findElement(By.cssSelector("input[name='bday']")).sendKeys("24-10-2025");
+
+        driver.findElement(By.cssSelector("input[value='Submit']")).click();
+        System.out.println(driver.findElement(By.cssSelector("div[class='alert alert-success alert-dismissible']")).getText());
+    }
+}
---
+++ alert.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class alert {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+        String text = "Krithvik";
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+
+        driver.findElement(By.cssSelector("input#name")).sendKeys(text);
+
+        driver.findElement(By.cssSelector("input#alertbtn")).click();
+
+        System.out.println(driver.switchTo().alert().getText()+" ***********");
+        driver.switchTo().alert().accept();
+
+        driver.findElement(By.cssSelector("input#confirmbtn")).click();
+        driver.switchTo().alert().dismiss();
+
+        
+    }
+    
+}
