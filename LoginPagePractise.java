322e59bc7160d4335fd4e25b484ece86a65972b3
abinayaavel <69348023+abinayaavel@users.noreply.github.com>
2025-07-30 15:21:16 +0530

    assignment and a wait scenarios

---
+++ LoginPagePractise.java
@@ -0,0 +1,66 @@
+package selenium;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class LoginPagePractise {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.get("https://rahulshettyacademy.com/loginpagePractise/");
+        LoginPagePractise loginPagePractise = new LoginPagePractise();
+        loginPagePractise.loginToPage(driver);
+
+    }
+
+    public void loginToPage(WebDriver driver) throws InterruptedException {
+
+        String userName = "rahulshettyacademy";
+        String pswd = "learning";
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(7000));
+
+        driver.findElement(By.cssSelector("input#username")).sendKeys(userName);
+        driver.findElement(By.cssSelector("input#password")).sendKeys(pswd);
+        driver.findElement(By.cssSelector("input[value='user']")).click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#okayBtn")));
+
+        driver.findElement(By.cssSelector("#okayBtn")).click();
+
+        Thread.sleep(3000);
+        Select option = new Select(driver.findElement(By.cssSelector("select.form-control")));
+        option.selectByValue("consult");
+
+        // span.termsText
+        driver.findElement(By.cssSelector("input#terms")).click();
+        driver.findElement(By.cssSelector("input#signInBtn")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.partialLinkText("Checkout")));
+
+        List<WebElement> titles = new ArrayList<>();
+        titles = driver.findElements(By.cssSelector("app-card-list.row div.card-body h4 a"));
+
+        for (int i = 0; i < titles.size(); i++) {
+            System.out.println(
+                    driver.findElements(By.cssSelector("app-card-list.row div.card-body h4 a")).get(i).getText());
+            driver.findElements(By.cssSelector("app-card-list.row div.card-footer button")).get(i)
+                    .click();
+
+        }
+
+        driver.findElement(By.partialLinkText("Checkout")).click();
+
+    }
+
+}
---
+++ fluentWait.java
@@ -0,0 +1,36 @@
+package selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Wait;
+import java.time.Duration;
+import java.util.NoSuchElementException;
+import java.util.function.Function;
+
+public class fluentWait {
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/dynamic_loading/1");
+        Thread.sleep(3000);
+        driver.findElement(By.cssSelector("div#start button")).click();
+
+        Wait<WebDriver> wait = new FluentWait<>(driver)
+                .withTimeout(Duration.ofSeconds(30))
+                .pollingEvery(Duration.ofSeconds(3))
+                .ignoring(NoSuchElementException.class);
+
+        // Use FluentWait to wait for an element to be clickable
+        wait.until(new Function<WebDriver, Boolean>() {
+            public Boolean apply(WebDriver driver) {
+                return driver.findElement(By.cssSelector("#finish")).isDisplayed();
+            }
+        });
+
+        driver.findElement(By.cssSelector("#finish")).click();
+
+        driver.quit();
+    }
+}
