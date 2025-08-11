8e9ff796df60176e49a724652551a4d0546eced2
abinayaavel <69348023+abinayaavel@users.noreply.github.com>
2025-08-11 13:29:24 +0530

    finding the broken links with soft assert concept

---
+++ brokenLink.java
@@ -0,0 +1,44 @@
+package selenium;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.asserts.SoftAssert;
+
+public class brokenLink {
+
+    public static void main(String[] args) throws MalformedURLException, IOException {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+
+        List<WebElement> links = driver.findElements(By.cssSelector("li.gf-li a"));
+        SoftAssert softAssert = new SoftAssert();
+
+        for (WebElement link : links) {
+
+            String URL = link.getAttribute("href");
+            HttpURLConnection conn = (HttpURLConnection) new URL(URL).openConnection();
+            conn.setRequestMethod("HEAD");
+            conn.connect();
+
+            int respCode = conn.getResponseCode();
+            System.out.println(respCode);
+
+            softAssert.assertTrue(respCode < 400,
+                    "The link with text " + link.getText() + " is broken with respnse code " + respCode);
+
+        }
+        softAssert.assertAll();
+
+    }
+
+}
