3f4d9c311f92f933929c3269ce1f160595151ef0
abinayaavel <69348023+abinayaavel@users.noreply.github.com>
2025-08-22 13:45:07 +0530

    relative locators intro

---
+++ RelativeLocator.java
@@ -0,0 +1,20 @@
+package selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import static org.openqa.selenium.support.locators.RelativeLocator.*;
+
+public class RelativeLocator {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/seleniumPractise/#/offers");
+        WebElement pageDropDown = driver.findElement(By.id("page-menu"));
+        System.out.println(driver.findElement(with(By.tagName("label")).near(pageDropDown)).getText());
+
+    }
+
+}
---
+++ sample.java
@@ -0,0 +1,49 @@
+package selenium;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class sample {
+
+    public static void main(String[] args) {
+
+        // get("Hello Wowhrld");
+        getUsingStreams("Hello Wowhrld");
+    }
+
+    private static void getUsingStreams(String input) {
+
+        input.replaceAll(" ", "");
+
+        Map<Character, Long> values = input.toLowerCase().chars().mapToObj(s -> (char) s)
+                .collect(Collectors.groupingBy(c -> c, Collectors.counting()));
+
+        values.forEach((k, v) -> System.out.println(k + ":" + v));
+    }
+
+    public static void get(String input) {
+
+        // List<Character> charList = input.chars().collect(Collectors.toList());
+
+        char[] charList = input.toLowerCase().toCharArray();
+        boolean[] b = new boolean[input.length()];
+
+        for (int i = 0; i < charList.length; i++) {
+            if (b[i])
+                continue;
+
+            // System.out.println(charList[i]);
+
+            int count = 1;
+            for (int j = i + 1; j < charList.length; j++) {
+                if (charList[i] == charList[j]) {
+
+                    count++;
+                    b[j] = true;
+                }
+            }
+            System.out.println(charList[i] + " = " + count);
+        }
+    }
+
+}
